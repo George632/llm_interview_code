65b59ebed070cb0dd9741c4112ed2fe05a51dcc9
George <77729650+George632@users.noreply.github.com>
2025-04-10 21:02:43 +0800

    Update LayerNorm.py

--- llm_components/LayerNorm.py
+++ llm_components/LayerNorm.py
@@ -1,1 +1,19 @@
+import torch
+import torch.nn as nn
 
+class LayerNorm(nn.Module):
+    def __init__(self, hidden_size: int, eps =1e-6):
+        super().__init__()
+        
+        self.gamma = nn.Parameter(torch.ones(hidden_size))
+        self.beta = nn.Parameter(torch.zeros(hidden_size))
+        self.eps = eps
+
+    def forward(self, x):
+        #求均值和方差
+        mean = torch.mean(x, dim=-1, keepdim=True) #[b, s, 1]
+        var= torch.var(x, dim=-1, keepdim=True, unbiased=False)
+        #正则化
+        x_normalized = (x - mean) / torch.sqrt(var + self.eps)
+        #加偏移
+        return self.gamma * x_normalized + self.beta
